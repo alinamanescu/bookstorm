b9a0f859fd1563def86a4d3515170ae17af78d65
alinamanescu <alidumi@gmail.com>
2024-04-17 22:06:08 +0300

    adding wishlist & cart

--- script.js
+++ script.js
@@ -10,17 +10,6 @@ document.querySelector('#close-login-btn').onclick = () => {
     loginForm.classList.remove('active');
 }
 
-window.onscroll = () => {
-
-    searchForm.classList.remove('active');
-
-    if(window.scrollY > 80){
-        document.querySelector('.header .header-2').classList.add('active');
-    }else{
-        document.querySelector('.header .header-2').classList.remove('active');
-    }
-
-}
 
 window.onload = () => {
 
@@ -31,6 +20,92 @@ window.onload = () => {
     }
 
 }
+
+let wishlistForm = document.querySelector('.wishlist');
+
+document.querySelector('#wishlist-btn').onclick = () => {
+   wishlistForm.classList.toggle('active');
+}
+
+document.querySelector('#close-wishlist-btn').onclick = () => {
+   wishlistForm.classList.remove('active');
+}
+
+
+let cartForm = document.querySelector('.cartItems');
+
+document.querySelector('#cart-btn').onclick = () => {
+   cartForm.classList.toggle('active');
+}
+
+document.querySelector('#close-cart-btn').onclick = () => {
+   cartForm.classList.remove('active');
+}
+
+
+// wishlist 
+
+function addToWishlist() {
+var wishlist = JSON.parse(localStorage.getItem('wishlist')) || [];
+
+if (!wishlist.includes(item)){
+  wishlist.push(item);
+
+  localStorage.setItem('wishlist',JSON.stringify(wishlist));
+  renderWishlist();
+} else {
+  alert(item + " is already in the wishlist.");
+}
+}
+
+function renderWishlist() {
+  var wishlistItemsElement = document.getElementById("wishlistItems");
+  wishlistItemsElement.innerHTML = "";
+
+  var wishlist = JSON.parse(localStorage.getItem('wishlist')) || [];
+
+  wishlist.forEach(function(item) {
+    var li = document.createElement("li");
+    li.textContent = item;
+    wishlistItemsElement.appendChild(li);
+  });
+}
+
+renderWishlist();
+
+// end of wishlist
+
+// cart
+
+
+function addToCart() {
+  var cart = JSON.parse(localStorage.getItem('cart')) || [];
+  
+  if (!cart.includes(item)){
+    cart.push(item);
+  
+    localStorage.setItem('cart',JSON.stringify(wishlist));
+    renderWishlist();
+  } 
+  }
+  
+  function renderCart() {
+    var cartItemsElement = document.getElementById("cartItems");
+    cartItemsElement.innerHTML = "";
+  
+    var cart = JSON.parse(localStorage.getItem('cart')) || [];
+  
+    cart.forEach(function(item) {
+      var li = document.createElement("li");
+      li.textContent = item;
+      cartItemsElement.appendChild(li);
+    });
+  }
+  
+  renderCart();
+
+// end of cart
+
 var swiper = new Swiper(".books-slider", {
     loop:true,
     centeredSlides: true,
@@ -60,9 +135,9 @@ var swiper = new Swiper(".books-slider", {
         disableOnInteraction: false,
     },
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
     breakpoints: {
       0: {
         slidesPerView: 1,
